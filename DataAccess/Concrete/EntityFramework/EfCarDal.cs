86227809359459be397caae2174933c32c761949
Hidayet ERDOĞAN <hido_904@hotmail.com>
2021-09-08 13:30:03 +0300

    8. Day For Rent A Car Project

---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,75 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : ICarDal
+    {
+        public void Add(Car entity)
+        {
+            if (entity.Description.Length>=2)
+            {
+                if (entity.DailyPrice>0)
+                {
+                    using (ReCapProjectContext context = new ReCapProjectContext())
+                    {
+                        var addedEntity = context.Entry(entity);
+                        addedEntity.State = EntityState.Added;
+                        context.SaveChanges();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Araç günlük ücreti 0'dan büyük olmalıdır.");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Araç ismi en az 2 karakterden oluşmalıdır.");
+            }
+        }
+
+        public void Delete(Car entity)
+        {
+            using (ReCapProjectContext context = new ReCapProjectContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            using (ReCapProjectContext context = new ReCapProjectContext())
+            {
+                return context.Set<Car>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            using (ReCapProjectContext context = new ReCapProjectContext())
+            {
+                return filter == null ? context.Set<Car>().ToList() : context.Set<Car>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Car entity)
+        {
+            using (ReCapProjectContext context = new ReCapProjectContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
@@ -49,6 +50,16 @@ namespace DataAccess.Concrete.InMemory
             carToUpdate.DailyPrice = car.DailyPrice;
             carToUpdate.Description = car.Description;
             carToUpdate.ModelYear = car.ModelYear;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
         }
     }
 }
